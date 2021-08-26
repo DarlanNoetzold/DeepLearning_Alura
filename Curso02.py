d2a894a197ebe09ff9b272e1c70e26dfb8ee3bef
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-08-26 07:15:17 -0300

    Aula 02

---
+++ Curso02.py
@@ -0,0 +1,72 @@
+import tensorflow
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.models import load_model
+
+dataset = keras.datasets.fashion_mnist
+((imagens_treino, identificacoes_treino), (imagens_teste, identificacoes_teste)) = dataset.load_data()
+
+len(imagens_treino)
+imagens_treino.shape
+imagens_teste.shape
+len(identificacoes_teste)
+identificacoes_treino.min()
+identificacoes_treino.max()
+
+total_de_classificacoes = 10
+nomes_de_classificacoes = ['Camiseta', 'Calça', 'Pullover',
+                           'Vestido', 'Casaco', 'Sandália', 'Camisa',
+                           'Tênis', 'Bolsa', 'Bota']
+
+plt.imshow(imagens_treino[0])
+plt.colorbar()
+
+imagens_treino = imagens_treino/float(255)
+
+modelo = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(256, activation=tensorflow.nn.relu),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(10, activation=tensorflow.nn.softmax)
+])
+
+modelo.compile(optimizer='adam',
+               loss='sparse_categorical_crossentropy',
+               metrics=['accuracy'])
+
+historico = modelo.fit(imagens_treino, identificacoes_treino, epochs=5, validation_split=0.2)
+
+sumario_do_modelo = modelo.summary()
+
+pesos_camada_dense = modelo.layers[1].get_weights()
+print(pesos_camada_dense)
+
+modelo.save('modelo.h5')
+modelo_salvo = load_model('modelo.h5')
+
+plt.plot(historico.history['acc'])
+plt.plot(historico.history['val_acc'])
+plt.title('Acurácia por épocas')
+plt.xlabel('épocas')
+plt.ylabel('acurácia')
+plt.legend(['treino', 'validação'])
+
+plt.plot(historico.history['loss'])
+plt.plot(historico.history['val_loss'])
+plt.title('Perda por épocas')
+plt.xlabel('épocas')
+plt.ylabel('perda')
+plt.legend(['treino', 'validação'])
+
+testes = modelo.predict(imagens_teste)
+print('resultado teste:', np.argmax(testes[1]))
+print('número da imagem de teste:', identificacoes_teste[1])
+
+testes_modelo_salvo = modelo_salvo.predict(imagens_teste)
+print('resultado teste modelo salvo:', np.argmax(testes_modelo_salvo[1]))
+print('número da imagem de teste:', identificacoes_teste[1])
+
+perda_teste, acuracia_teste = modelo.evaluate(imagens_teste, identificacoes_teste)
+print('Perda do teste:', perda_teste)
+print('Acurácia do teste:', acuracia_teste)
