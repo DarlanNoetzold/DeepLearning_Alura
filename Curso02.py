92273f4aa8bdf6bd893ccf9b3ee6a14cf51a3311
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-08-29 07:56:02 -0300

    Update Curso02.py

--- Curso02.py
+++ Curso02.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow.keras.models import load_model
 
+#Carregando o dataset
 dataset = keras.datasets.fashion_mnist
 ((imagens_treino, identificacoes_treino), (imagens_teste, identificacoes_teste)) = dataset.load_data()
 
+#Exploração dos dados
 len(imagens_treino)
 imagens_treino.shape
 imagens_teste.shape
@@ -14,6 +16,7 @@ len(identificacoes_teste)
 identificacoes_treino.min()
 identificacoes_treino.max()
 
+#Exibição dos dados
 total_de_classificacoes = 10
 nomes_de_classificacoes = ['Camiseta', 'Calça', 'Pullover',
                            'Vestido', 'Casaco', 'Sandália', 'Camisa',
@@ -22,8 +25,10 @@ nomes_de_classificacoes = ['Camiseta', 'Calça', 'Pullover',
 plt.imshow(imagens_treino[0])
 plt.colorbar()
 
+#Normalizando as imagens
 imagens_treino = imagens_treino/float(255)
 
+#Criando, compilando, treinando e normalizando o modelo
 modelo = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(256, activation=tensorflow.nn.relu),
@@ -31,21 +36,53 @@ modelo = keras.Sequential([
     keras.layers.Dense(10, activation=tensorflow.nn.softmax)
 ])
 
+adam = keras.optimizers.Adam(lr=0.002)
+
+'''lr
+learning rate
+taxa de aprendizado'''
+
+parando_cedo = [keras.callbacks.EarlyStopping(monitor='val_loss'),
+                keras.callbacks.ModelCheckpoint(filepath='melhor_modelo.hdf5',
+                                               monitor='val_loss', save_best_only=True)]
+
+modelo.compile(optimizer=adam,
+               loss='sparse_categorical_crossentropy',
+               metrics=['accuracy'])
+
+historico = modelo.fit(imagens_treino, identificacoes_treino,
+                       batch_size = 480,
+                       epochs=5, validation_split=0.2,
+                       callbacks=parando_cedo)
+
+sumario_do_modelo = modelo.summary()
+
+#Pesos da primeira camada Dense
+pesos_camada_dense = modelo.layers[1].get_weights()[0]
+print(pesos_camada_dense)
+
+print(pesos_camada_dense.shape)
+pesos_camada_dense_zerados = np.zeros((784, 256))
+
+pesos_camada_dense_aleatorios = np.random.rand(784, 256)
+
+'''
+modelo.layers[1].set_weights(
+    [pesos_camada_dense_zerados, vieses_camada_dense]
+)
+'''
+
+modelo.layers[1].set_weights(
+    [pesos_camada_dense_aleatorios, vieses_camada_dense]
+)
+
+modelo.layers[1].get_weights()
 modelo.compile(optimizer='adam',
                loss='sparse_categorical_crossentropy',
                metrics=['accuracy'])
 
 historico = modelo.fit(imagens_treino, identificacoes_treino, epochs=5, validation_split=0.2)
 
-sumario_do_modelo = modelo.summary()
-
-pesos_camada_dense = modelo.layers[1].get_weights()
-print(pesos_camada_dense)
-
-modelo.save('modelo.h5')
-modelo_salvo = load_model('modelo.h5')
-
-modelo.get_config()
 
 plt.plot(historico.history['acc'])
 plt.plot(historico.history['val_acc'])
@@ -54,6 +91,43 @@ plt.xlabel('épocas')
 plt.ylabel('acurácia')
 plt.legend(['treino', 'validação'])
 
+
+plt.plot(historico.history['acc'])
+plt.plot(historico.history['val_acc'])
+plt.title('Acurácia por épocas')
+plt.xlabel('épocas')
+plt.ylabel('acurácia')
+plt.legend(['treino', 'validação'])
+modelo.get_config()
+
+
+plt.plot(historico.history['loss'])
+plt.plot(historico.history['val_loss'])
+plt.title('Perda por épocas')
+plt.xlabel('épocas')
+plt.ylabel('perda')
+plt.legend(['treino', 'validação'])
+vieses_camada_dense = modelo.layers[1].get_weights()[1]
+print(vieses_camada_dense.shape)
+
+vieses_camada_dense_zerados = np.zeros((256,))
+modelo.layers[1].set_weights(
+    [pesos_camada_dense_aleatorios, vieses_camada_dense_zerados]
+)
+
+#Salvando e carregando o modelo treinado
+modelo.save('modelo.h5')
+modelo_salvo = load_model('modelo.h5')
+
+#Visualizando as acurácias de treino e validação por época
+plt.plot(historico.history['acc'])
+plt.plot(historico.history['val_acc'])
+plt.title('Acurácia por épocas')
+plt.xlabel('épocas')
+plt.ylabel('acurácia')
+plt.legend(['treino', 'validação'])
+
+#Visualizando as perdas de treino e validação por época
 plt.plot(historico.history['loss'])
 plt.plot(historico.history['val_loss'])
 plt.title('Perda por épocas')
@@ -61,6 +135,7 @@ plt.xlabel('épocas')
 plt.ylabel('perda')
 plt.legend(['treino', 'validação'])
 
+#Testando o modelo e o modelo salvo
 testes = modelo.predict(imagens_teste)
 print('resultado teste:', np.argmax(testes[1]))
 print('número da imagem de teste:', identificacoes_teste[1])
@@ -69,8 +144,7 @@ testes_modelo_salvo = modelo_salvo.predict(imagens_teste)
 print('resultado teste modelo salvo:', np.argmax(testes_modelo_salvo[1]))
 print('número da imagem de teste:', identificacoes_teste[1])
 
+#Avaliando o modelo
 perda_teste, acuracia_teste = modelo.evaluate(imagens_teste, identificacoes_teste)
 print('Perda do teste:', perda_teste)
 print('Acurácia do teste:', acuracia_teste)
-
-#Gradiente Descendente, Minimo local, minimo global e pontos aleatórios de inicio para desviar dos minimos locais (gradiente descendente estocástico)
