14613fbb5869a917b0cabd55eab7bbc53792e04b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-09-06 06:54:37 -0300

    Aula 01,02 e 03

---
+++ Curso04.py
@@ -0,0 +1,172 @@
+# A base de dados
+
+### Carregando o dataset
+
+from google.colab import files
+
+files.upload()
+
+import pandas as pd
+
+dados = pd.read_csv('Bicicletas.csv')
+
+### Conhecendo a base de dados
+
+dados.head()
+
+dados.shape
+
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({'font.size': 14})
+plt.scatter(dados['temperatura'], dados['bicicletas_alugadas'])
+plt.ylabel('bicicletas_alugadas')
+plt.xlabel('temperatura')
+
+plt.scatter(dados['clima'], dados['bicicletas_alugadas'])
+plt.ylabel('bicicletas_alugadas')
+plt.xlabel('clima')
+plt.rcParams.update({'font.size': 22})
+indice = [1, 2, 3]
+plt.xticks(indice, fontsize=14)
+
+### Normalizando a base de dados
+
+import numpy as np
+
+y = dados['bicicletas_alugadas'].values
+
+X = dados[['clima', 'temperatura']].values
+print(X)
+
+X = X / np.amax(X, axis=0)
+print(X)
+
+ymax = np.amax(y)
+y = y / ymax
+print(y[0:10])
+
+
+### Funções de ativação
+
+def sigmoid(Soma):
+    return 1 / (1 + np.exp(-Soma))
+
+
+def relu(Soma):
+    return np.maximum(0, Soma)
+
+
+### Criando a estrutura da rede
+
+arquitetura = [
+    {"dim_entrada": 2, "dim_saida": 50, "ativacao": "relu"},
+    {"dim_entrada": 50, "dim_saida": 1, "ativacao": "sigmoid"},
+]
+
+
+### Pesos e viés
+
+def inicia_camadas(arquitetura, seed=99):
+    # inicia os valores aleatórios
+    np.random.seed(seed)
+    # numero de camadas da rede neural
+    numero_de_camadas = len(arquitetura)
+    # inicia armazenamento de parametros
+    valores_parametros = {}
+
+    # itera nas camadas da rede
+    for indice, camada in enumerate(arquitetura):
+        indice_camada = indice + 1
+
+        # extrai o numero de nodos nas camadas
+        tamanho_camada_entrada = camada["dim_entrada"]
+        tamanho_camada_saida = camada["dim_saida"]
+
+        # inicia os valores na matriz de pesos P
+        # e o vetor de viés ou bias b
+        valores_parametros['P' + str(indice_camada)] = np.random.randn(
+            tamanho_camada_saida, tamanho_camada_entrada) * 0.1
+        valores_parametros['b' + str(indice_camada)] = np.random.randn(
+            tamanho_camada_saida, 1) * 0.1
+
+    return valores_parametros
+
+
+### Propagação da rede
+
+def propaga_uma_camada(Ativado_anterior, Pesos_atual, b_atual, ativacao="relu"):
+    # cálculo da entrada para a função de ativação
+    Saida_atual = np.dot(Pesos_atual, Ativado_anterior) + b_atual
+
+    # selecção da função de ativação
+    if ativacao is "relu":
+        func_ativacao = relu
+    elif ativacao is "sigmoid":
+        func_ativacao = sigmoid
+    else:
+        raise Exception('Ainda não implementamos essa funcao')
+
+    # retorna a ativação calculada Ativado_atual e a matriz intermediária Saida
+    return func_ativacao(Saida_atual), Saida_atual
+
+
+def propaga_total(X, valores_parametros, arquitetura):
+    # memoria temporaria para a retropropagacao
+    memoria = {}
+    # O vetor X é a ativação para a camada 0 
+    Ativado_atual = X
+
+    # iterações para as camadas
+    for indice, camada in enumerate(arquitetura):
+        # a numeração das camadas começa de 1
+        indice_camada = indice + 1
+        # utiliza a ativação da iteração anterior
+        Ativado_anterior = Ativado_atual
+
+        # extrai a função de ativação para a camada atual
+        func_ativacao_atual = camada["ativacao"]
+        # extrai os pesos da camada atual
+        Pesos_atual = valores_parametros["P" + str(indice_camada)]
+        # extrai o bias para a camada atual
+        b_atual = valores_parametros["b" + str(indice_camada)]
+        # cálculo da ativação para a camada atual
+        Ativado_atual, Saida_atual = propaga_uma_camada(Ativado_anterior, Pesos_atual, b_atual, func_ativacao_atual)
+
+        # salca os valores calculados na memória
+        memoria["A" + str(indice)] = Ativado_anterior
+        memoria["Z" + str(indice_camada)] = Saida_atual
+
+    # retorna o vetor predito e um dicionário contendo os valores intermediários
+    return Ativado_atual, memoria
+
+
+### Testando a rede
+
+valores_parametros = inicia_camadas(arquitetura, seed=99)
+y_estimado, memoria = propaga_total(np.transpose(X), valores_parametros, arquitetura)
+
+y_estimado[0, 0] * ymax
+
+y[0] * ymax
+
+
+### Atualização dos pesos
+
+def atualiza(valores_parametros, gradidentes, arquitetura, taxa_aprendizagem):
+    # iterações pelas camadas
+    for indice_camada, camada in enumerate(arquitetura, 1):
+        valores_parametros["P" + str(indice_camada)] -= taxa_aprendizagem * gradidentes["dP" + str(indice_camada)]
+        valores_parametros["b" + str(indice_camada)] -= taxa_aprendizagem * gradidentes["db" + str(indice_camada)]
+
+    return valores_parametros;
+
+
+### Função de custo
+
+def valor_de_custo(Y_predito, Y):
+    # numero_de_exemplos
+    m = Y_predito.shape[1]
+
+    custo = -1 / m * (np.dot(Y, np.log(Y_predito).T) + np.dot(1 - Y, np.log(1 - Y_predito).T))
+    return np.squeeze(custo)
