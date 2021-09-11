09ef020e18feb0db4c63898c63e4ba7d4798f501
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-09-11 07:42:58 -0300

    Update Curso05.py

--- Curso05.py
+++ Curso05.py
@@ -27,5 +27,35 @@ print(resenha.head())
 
 classificacao = resenha["sentiment"].replace(["neg", "pos"], [0,1])
 resenha["classificacao"] = classificacao
-
 print(resenha.tail())
+
+#Aula 2 - Bag of Words: criando representações da linguagem humana.
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+texto = ["Assisti um filme ótimo", "Assisti um filme ruim"]
+
+vetorizar = CountVectorizer(lowercase=False)
+bag_of_words = vetorizar.fit_transform(texto)
+vetorizar.get_feature_names()
+
+print(bag_of_words)
+
+matriz_esparsa = pd.SparseDataFrame(bag_of_words,
+                      columns=vetorizar.get_feature_names())
+
+vetorizar = CountVectorizer(lowercase=False, max_features=50)
+bag_of_words = vetorizar.fit_transform(resenha.text_pt)
+print(bag_of_words.shape)
+
+def classificar_texto(texto, coluna_texto, coluna_classificacao):
+    vetorizar = CountVectorizer(lowercase=False, max_features=50)
+    bag_of_words = vetorizar.fit_transform(texto[coluna_texto])
+    treino, teste, classe_treino, classe_teste = train_test_split(bag_of_words,
+                                                              texto[coluna_classificacao],
+                                                              random_state = 42)
+    regressao_logistica = LogisticRegression(solver = "lbfgs")
+    regressao_logistica.fit(treino, classe_treino)
+    return regressao_logistica.score(teste, classe_teste)
+
+print(classificar_texto(resenha, "text_pt", "classificacao"))
