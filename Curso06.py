b3c56e21f72db010235e68b5684f35f3154fb54c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-09-21 07:06:50 -0300

    Update Curso06.py

--- Curso06.py
+++ Curso06.py
@@ -4,3 +4,27 @@ dados_portugues.head()
 
 questao_portugues = dados_portugues.Questão[5]
 print(questao_portugues)
+
+dados_ingles = pd.read_csv("stackoverflow_ingles.csv")
+dados_ingles.head()
+
+questao_ingles = dados_ingles.Questão[0]
+print(questao_ingles)
+
+import re
+re.findall(r"<.*?>",questao_portugues)
+print(questao_portugues)
+
+texto_teste = re.sub(r"<.*?>","  T----E----S----T----E  ",questao_portugues)
+print(texto_teste)
+
+re.search(r"70","18728736187263817628631872638716283670")
+regex = re.compile(r"70")
+regex.search("18728736187263817628631872638716283670")
+from timeit import timeit
+setup = """import re"""
+timeit("""re.search(r"70","18728736187263817628631872638716283670")""", setup)
+setup = """import re
+regex = re.compile(r"70")"""
+
+timeit("""regex.search("18728736187263817628631872638716283670")""", setup)
