ad715b3b6f7530d7dbd0453f98ba97a6079a2461
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-10-04 07:20:11 -0300

    Create Curso08.py

---
+++ Curso08.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import spacy
+
+dados_treino = pd.read_csv("/content/drive/My Drive/curso_word2vec/treino.csv")
+dados_treino.sample(5)
+
+nlp = spacy.load("pt_core_news_sm")
+
+
+texto = "Rio de Janeiro é uma cidade maravilhosa"
+doc = nlp(texto
