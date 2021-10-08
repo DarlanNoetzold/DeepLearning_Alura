9928fea4483c0b2b85ab6ef7f0584e4c95657e36
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-10-08 07:30:18 -0300

    Update Curso08.py

--- Curso08.py
+++ Curso08.py
@@ -88,3 +88,66 @@ w2v_modelo_sg.train(lista_lista_tokens,
 
 w2v_modelo_sg.wv.most_similar("google")
 
+##################################################################################################
+#!python -m spacy download pt_core_news_sm      --rodar no colab
+
+import spacy
+import numpy as np
+import pandas as pd
+from gensim.models import KeyedVectors
+
+w2v_modelo_cbow = KeyedVectors.load_word2vec_format("/content/drive/My Drive/curso_word2vec/modelo_cbow.txt")
+w2v_modelo_sg = KeyedVectors.load_word2vec_format("/content/drive/My Drive/curso_word2vec/modelo_skipgram.txt")
+artigo_treino = pd.read_csv("/content/drive/My Drive/curso_word2vec/treino.csv")
+artigo_teste = pd.read_csv("/content/drive/My Drive/curso_word2vec/teste.csv")
+
+nlp = spacy.load("pt_core_news_sm", disable=["paser", "ner", "tagger", "textcat"])
+
+def tokenizador(texto):
+    doc = nlp(texto)
+    tokens_validos = []
+    for token in doc:
+        e_valido = not token.is_stop and token.is_alpha
+        if e_valido:
+            tokens_validos.append(token.text.lower())
+
+    return tokens_validos
+
+
+texto = "Rio de Janeiro 1231231 ***** @#$ é uma cidade maravilhosa!"
+tokens = tokenizador(texto)
+print(tokens)
+
+
+def combinacao_de_vetores_por_soma(palavras, modelo):
+    vetor_resultante = np.zeros((1, 300))
+
+    for pn in palavras:
+        try:
+            vetor_resultante += modelo.get_vector(pn)
+
+        except KeyError:
+            pass
+
+    return vetor_resultante
+
+
+vetor_texto = combinacao_de_vetores_por_soma(tokens, w2v_modelo_cbow)
+print(vetor_texto.shape)
+print(vetor_texto)
+
+def matriz_vetores(textos, modelo):
+    x = len(textos)
+    y = 300
+    matriz = np.zeros((x,y))
+
+    for i in range(x):
+        palavras = tokenizador(textos.iloc[i])
+        matriz[i] = combinacao_de_vetores_por_soma(palavras, modelo)
+
+    return matriz
+
+matriz_vetores_treino_cbow = matriz_vetores(artigo_treino.title, w2v_modelo_cbow)
+matriz_vetores_teste_cbow = matriz_vetores(artigo_teste.title, w2v_modelo_cbow)
+print(matriz_vetores_treino_cbow.shape)
+print(matriz_vetores_teste_cbow.shape)
