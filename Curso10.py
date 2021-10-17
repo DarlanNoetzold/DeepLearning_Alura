247a1c075fa51dd1dc8847666608a936e0b2ee61
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-10-17 07:18:54 -0300

    Update Curso10.py

--- Curso10.py
+++ Curso10.py
@@ -25,3 +25,82 @@ for (x,y,w,h) in faces:
     imagem_roi = imagem[y:y+h, x:x+w]
     imagem_roi = cv2.cvtColor(imagem_roi, cv2.COLOR_RGB2BGR)
     cv2.imwrite("face_" + str(face_imagem) + ".png", imagem_roi)
+
+
+from os import listdir, path, makedirs
+from os.path import isfile, join
+
+import shutil
+
+imagem_face_1 = cv2.imread("imagens/cropped_faces/s01_01.jpg")
+imagem_face_1 = cv2.cvtColor(imagem_face_1, cv2.COLOR_BGR2RGB)
+
+imagem_face_2 = cv2.imread("imagens/cropped_faces/s02_01.jpg")
+imagem_face_2 = cv2.cvtColor(imagem_face_2, cv2.COLOR_BGR2RGB)
+
+imagem_face_3 = cv2.imread("imagens/cropped_faces/s03_01.jpg")
+imagem_face_3 = cv2.cvtColor(imagem_face_3, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(20,10))
+plt.subplot(131)
+plt.title("Sujeito 01")
+plt.imshow(imagem_face_1)
+plt.subplot(132)
+plt.title("Sujeito 02")
+plt.imshow(imagem_face_2)
+plt.subplot(133)
+plt.title("Sujeito 03")
+plt.imshow(imagem_face_3)
+
+plt.show()
+
+faces_caminho = "imagens/cropped_faces/"
+lista_arq_faces = [f for f in listdir(faces_caminho) if isfile(join(faces_caminho, f))]
+
+faces_path_treino = "imagens/treino/"
+faces_path_teste = "imagens/teste/"
+
+if not path.exists(faces_path_treino):
+    makedirs(faces_path_treino)
+
+if not path.exists(faces_path_teste):
+    makedirs(faces_path_teste)
+
+for arq in lista_arq_faces:
+    sujeito = arq[1:3]
+    numero = arq[4:6]
+
+    if int(numero) <= 10:
+        shutil.copyfile(faces_caminho + arq, faces_path_treino + arq)
+    else:
+        shutil.copyfile(faces_caminho + arq, faces_path_teste + arq)
+
+def padronizar_imagem(imagem_caminho):
+    imagem = cv2.imread(imagem_caminho, cv2.IMREAD_GRAYSCALE)
+    imagem = cv2.resize(imagem, (200, 200), interpolation=cv2.INTER_LANCZOS4)
+    return imagem
+
+lista_faces_treino = [f for f in listdir(faces_path_treino) if isfile(join(faces_path_treino, f))]
+lista_faces_teste = [f for f in listdir(faces_path_teste) if isfile(join(faces_path_teste, f))]
+
+dados_treinamento, sujeitos = [], []
+for i, arq in enumerate(lista_faces_treino):
+    imagem_path = faces_path_treino + arq
+    imagem = padronizar_imagem(imagem_path)
+    dados_treinamento.append(imagem)
+    sujeito = arq[1:3]
+    sujeitos.append(int(sujeito))
+
+dados_teste, sujeitos_teste = [], []
+for i, arq in enumerate(lista_faces_teste):
+    imagem_path = faces_path_teste + arq
+    imagem = padronizar_imagem(imagem_path)
+    dados_teste.append(imagem)
+    sujeito = arq[1:3]
+    sujeitos_teste.append(int(sujeito))
+
+plt.imshow(dados_treinamento[0], cmap="gray")
+plt.title(sujeito[0])
+
+plt.imshow(dados_teste[0], cmap="gray")
+plt.title(sujeitos_teste[0])
