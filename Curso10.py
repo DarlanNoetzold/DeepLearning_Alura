07561bd58e522404f4f37118ba12363448d2ec64
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-10-19 07:43:31 -0300

    Update Curso10.py

--- Curso10.py
+++ Curso10.py
@@ -183,3 +183,138 @@ for item in dados_teste:
     y_pred_lbph.append(modelo_lbph.predict(item)[0])
 acuracia_lbph = accuracy_score(sujeitos_teste, y_pred_lbph)
 
+import dlib
+import matplotlib.pyplot as plt
+
+from scipy.spatial import distance as dist
+
+imagem = cv2.imread("imagens/px-woman-smilings.jpg")
+imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(20,10))
+plt.imshow(imagem)
+
+classificador_dlib_68_path = "classificadores/shape_predictor_68_face_landmarks.dat"
+classificador_dlib = dlib.shape_predictor(classificador_dlib_68_path)
+detector_face = dlib.get_frontal_face_detector()
+
+
+def anotar_rosto(imagem):
+    retangulos = detector_face(imagem, 1)
+
+    if len(retangulos) == 0:
+        return None
+
+    for k, d in enumerate(retangulos):
+        print("Identificado rosto " + str(k))
+        cv2.rectangle(imagem, (d.left(), d.top()), (d.right(), d.bottom()), (255, 255, 0), 2)
+
+    return imagem
+
+imagem_anotada = imagem.copy()
+imagem_anotada = anotar_rosto(imagem_anotada)
+
+plt.figure(figsize=(20,10))
+plt.imshow(imagem_anotada)
+
+
+def pontos_marcos_faciais(imagem):
+    retangulos = detector_face(imagem, 1)
+
+    if len(retangulos) == 0:
+        return None
+
+    marcos = []
+
+    for ret in retangulos:
+        marcos.append(np.matrix([[p.x, p.y] for p in classificador_dlib(imagem, ret).parts()]))
+
+    return marcos
+
+marcos_faciais = pontos_marcos_faciais(imagem)
+len(marcos_faciais)
+len(marcos_faciais[0])
+
+def anotar_marcos_faciais(imagem, marcos):
+    for marco in marcos:
+        for idx, ponto in enumerate(marco):
+            centro = (ponto[0, 0], ponto[0, 1])
+            cv2.circle(imagem, centro, 3, (255, 255, 0), -1)
+            cv2.putText(imagem, str(idx), centro, cv2.FONT_HERSHEY_SIMPLEX, 0.8,
+                        (255, 255, 255), 2)
+
+    return imagem
+
+imagem_anotada = imagem.copy()
+imagem_anotada = anotar_marcos_faciais(imagem_anotada, marcos_faciais)
+
+plt.figure(figsize=(20,10))
+plt.imshow(imagem_anotada)
+
+imagem_pessoa = cv2.imread("imagens/px-man-happy.jpg")
+imagem_pessoa = cv2.cvtColor(imagem_pessoa, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(20,10))
+plt.imshow(imagem_pessoa)
+
+FACE = list(range(17, 68))
+FACE_COMPLETA = list(range(0, 68))
+LABIO = list(range(48, 61))
+SOMBRANCELHA_DIRETA = list(range(17, 22))
+SOMBRANCELHA_ESQUERDA = list(range(22, 27))
+OLHO_DIREITO = list(range(36,42))
+OLHO_ESQUERDO = list(range(42,48))
+NARIZ = list(range(27,35))
+MANDIBULA = list(range(0,17))
+
+
+def aspecto_razao_olhos(pontos_olhos):
+    a = dist.euclidean(pontos_olhos[1], pontos_olhos[5])
+    b = dist.euclidean(pontos_olhos[2], pontos_olhos[4])
+    c = dist.euclidean(pontos_olhos[0], pontos_olhos[3])
+
+    aspecto_razao = (a + b) / (2.0 * c)
+
+    return aspecto_razao
+
+
+def anotar_marcos_casca_convexa(imagem, marcos):
+    retangulos = detector_face(imagem, 1)
+
+    if len(retangulos) == 0:
+        return None
+
+    for idx, ret in enumerate(retangulos):
+        marco = marcos[idx]
+
+        pontos = cv2.convexHull(marco[OLHO_ESQUERDO])
+        cv2.drawContours(imagem, [pontos], 0, (0, 255, 0), 2)
+
+        pontos = cv2.convexHull(marco[OLHO_DIREITO])
+        cv2.drawContours(imagem, [pontos], 0, (0, 255, 0), 2)
+
+    return imagem
+
+imagem_anotada = imagem_pessoa.copy()
+imagem_anotada = anotar_marcos_casca_convexa(imagem_anotada, marcos_faciais)
+
+plt.figure(figsize=(20,10))
+plt.imshow(imagem_anotada)
+
+valor_olho_esquerdo = aspecto_razao_olhos(marcos_faciais[0][OLHO_ESQUERDO])
+valor_olho_direito = aspecto_razao_olhos(marcos_faciais[0][OLHO_DIREITO])
+imagem_pessoa_seria = cv2.imread("imagens/px-man-serious.jpg")
+imagem_pessoa_seria = cv2.cvtColor(imagem_pessoa_seria, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(20,10))
+plt.imshow(imagem_pessoa_seria)
+
+marcos_faciais = pontos_marcos_faciais(imagem_pessoa_seria)
+imagem_anotada = imagem_pessoa_seria.copy()
+imagem_anotada = anotar_marcos_casca_convexa(imagem_anotada, marcos_faciais)
+
+plt.figure(figsize=(20,10))
+plt.imshow(imagem_anotada)
+
+valor_olho_esquerdo = aspecto_razao_olhos(marcos_faciais[0][OLHO_ESQUERDO])
+valor_olho_direito = aspecto_razao_olhos(marcos_faciais[0][OLHO_DIREITO])
